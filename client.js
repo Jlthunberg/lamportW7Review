d30a9387194c77a88d774c1f1274ca37c971f98d
Dev Jana <devjana@Devs-MBP-2.domain>
2020-04-10 10:50:01 -0500

    input working

--- client.js
+++ client.js
@@ -1,5 +1,22 @@
 $( document ).ready( onReady );
 
+let inventory = [];
+
+function addItem(){
+    console.log( 'in addItem' );
+    // get user inputs & place into an object
+    let itemToAdd = {
+        description: $( '#descriptionIn' ).val(),
+        color: $( '#colorIn' ).val(),
+        size: $( '#sizeIn' ).val()
+    } // end itemToAdd
+    console.log( 'itemToAdd:', itemToAdd );
+    // push object into our inventory array
+    inventory.push( itemToAdd );
+    /// - TEMP
+    console.log( 'inventory:', inventory );
+} // end addItem
+
 function onReady(){
-    console.log( 'in onReady' );
+    $( '#addItemButton' ).on( 'click', addItem );
 } // end onReady
---
+++ readme.md
@@ -0,0 +1,9 @@
+Lamport W7 Review App
+===
+
+Inventory app for an oddities store. 
+
+[x] add items to inventory (description, color, size)
+[ ] view items in inventory
+[ ] search for items by color + size
+[ ] view search results
